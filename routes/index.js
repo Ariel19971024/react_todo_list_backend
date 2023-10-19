01aea16c5055ad5af5518147755ba18e0d5b590d
Ariel1997 <life23913349@gmail.com>
2023-10-19 16:44:03 +0800

    feat: Build update api

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,20 @@ const { v4: uuidv4 } = require("uuid");
 const List = require("../schema/todo.js");
 var router = express.Router();
 
-/* GET home page. */
-let todo = [
-  {
-    id: uuidv4(),
-    text: "do something",
-    date: "2023-10-24",
-  },
-];
-
 router
   .get("/todo", (req, res) => {
     List.find({})
       .exec()
       .then((data) => {
-        res.send(data);
-      }).catch((e)=>{
-        res.status(400).send(e)
+        res.send({ status: "200", data, msg: "success" });
+      })
+      .catch((e) => {
+        res.status(400).send(e);
       });
-  
   })
 
   .post("/todo", async (req, res) => {
     try {
-      console.log({ ...req.body, created_date: new Date() });
       const user = new List({
         id: uuidv4(),
         ...req.body,
@@ -39,9 +29,23 @@ router
       res.status(404).send(e);
     }
   })
-  .delete('/todo/:id',(req,res)=>{
-    console.log(req)
+  .delete("/todo/:id", (req, res) => {
+    //可以拆成Controller
+    List.deleteOne({ id: req.params.id }).then((msg) => {
+      res.send({ status: "200", msg: "delete success" });
+    });
   })
-  ;
+  .put("/todo/:id", (req, res) => {
+    List.updateOne(
+      {
+        id: req.params.id,
+      },
+      {
+        text: req.body.text,
+      }
+    ).then((data) => {
+      res.send({ status: "200", msg: "update success" });
+    });
+  });
 
 module.exports = router;
