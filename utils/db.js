67bf0201c281ec3ca37a12bf3fe9e231d82e5906
Ariel1997 <life23913349@gmail.com>
2023-10-22 14:44:59 +0800

    feat: update procfile space

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose
-  .connect(process.env.DB_HOST, {})
+  .connect(process.env.MONGODB_URL || process.env.DB_HOST, {})
   .then(() => console.log("DB connection successful!"));
   module.exports = mongoose
